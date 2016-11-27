5b261000f7b6ec8b795bd4813fb0a90262cad6f6
Harshit <pantharshit00@gmail.com>
2016-11-27 13:46:46 +0530

    Articles

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var pug = require('pug');
+var app = express();
+app.use(morgan('dev'));
+app.use('/static', express.static('static'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.render('index', {title: "Home | Articles"});
+});
+
+app.get('/:filename.png', function (req, res) {
+    res.sendFile(path.join(__dirname, req.params.filename + '.png'));
+});
+
+app.get('/:filename.json', function (req, res) {
+    res.sendFile(path.join(__dirname, req.params.filename + '.json'));
+});
+
+app.get('/:filename.svg', function (req, res) {
+    res.sendFile(path.join(__dirname, req.params.filename + '.svg'));
+});
+
+app.get('/:filename.xml', function (req, res) {
+    res.sendFile(path.join(__dirname, req.params.filename + '.xml'));
+});
+
+app.use(function (req, res) {
+    res.status(404).render('error', {message: "Cannot load the destination", error: "Error 404"});
+});
+
+app.listen(8080, function () {
+    console.log("app started");
+});
+
+
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+console.log('JADE is working YEAH HEY');
