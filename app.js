90843950be4512476c6f59624dc30e16f7c12a99
Harshit <pantharshit00@gmail.com>
2016-12-06 19:33:30 +0530

    Articles

--- app.js
+++ app.js
@@ -2,10 +2,69 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pug = require('pug');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var crypto = require('crypto')
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var Sequelize = require('sequelize');
+var connection = new Sequelize('pantharshit00', 'pantharshit00', process.env.DB_password, {
+    host: 'db.imad.hasura-app.io',
+    dialect: 'postgres'
+});
+
+var User = connection.define('user',{
+    id:{type:Sequelize.INTEGER,autoIncrement:true, primaryKey:true},
+   name: Sequelize.STRING,
+    email:{ type: Sequelize.STRING , unique: true } ,
+    password:Sequelize.TEXT
+
+});
+
+connection.sync();
+
+
+
+
+
+
+
+
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 var app = express();
-app.use(morgan('combined'));
+
+
+app.use(cookieParser());
+app.use(morgan('dev'));
 app.use('/static', express.static('static'));
 app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true , maxAge: 2592000000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+function validatePassword(password,originalPassword){
+    var salt = originalPassword.split('$')[0];
+  var submittedPasswordHash = crypto.pbkdf2Sync(password,salt,10000,64,'sha512').toString('hex');
+    if(submittedPasswordHash === originalPassword.split('$')[1])
+        return true;
+    else
+        return false;
+};
+
 
 app.get('/', function (req, res) {
     res.render('index', {title: "Home | Articles"});
@@ -30,10 +89,59 @@ app.get('/:filename.xml', function (req, res) {
 app.get('/login', function(req,res){
    res.render('login',{title: "Login | Articles"});
 });
-
+var errors = {};
 app.get('/register',function(req,res){
    res.render('register',{title:"Sign Up | Articles"});
 });
+
+function createHash(text){
+    var salt = crypto.randomBytes(16).toString('hex');
+    var hashed = crypto.pbkdf2Sync(text,salt,10000,64,'sha512').toString('hex');
+    return [salt,hashed].join('$');
+
+};
+
+app.post('/signup',function (req,res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Confirm Your Password').notEmpty();
+    req.checkBody('password2', 'Passwords Do not match').equals(req.body.password);
+    var emailAvailable = true;
+    errors = req.validationErrors();
+    if (errors) {
+        res.render('register', {
+            errors: errors
+
+        })
+        ;
+    } else if(!errors) {
+        User.findOne({
+            where: {email: email}
+        }).then(function () {
+            emailAvailable = false;
+            res.status(403).render('register',{
+                errors:[{msg:"Email is alredy in use"}]
+        })})}
+
+      else if(emailAvailable){
+        connection.sync().then(function () {
+            User.create({
+                name: name,
+                email: email,
+                password: createHash(password)
+            })
+                res.redirect('/login');
+            }
+)};
+});
+
 
 app.use(function (req, res) {
     res.status(404).render('error', {message: "Cannot load the destination", error: "Error 404"});
--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,14 @@ function iconFix(x) {
 
 function menuFix() {
     var x = document.getElementsByClassName("men")[0];
+    var con = document.getElementById('con');
     if (x.className === "men") {
         x.className += " responsive";
         x.style.display = 'inline-block';
+        con.style.display = 'none';
     } else {
         x.style.display = 'none'
         x.className = 'men';
+        con.style.display = 'block';
     }
 }
